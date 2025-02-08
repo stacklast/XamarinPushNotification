87a50c7ac6b75922b7f68795d275d4c5218d0415
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-02-08 14:12:26 -0500

    Add project files.

---
+++ XamarinPushNotification.Android/MyFirebaseMessagingService.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Util;
+using Firebase.Messaging;
+using Xamarin.Essentials;
+
+namespace XamarinPushNotification.Droid
+{
+    [Service(Exported = true)] // 👈 Agrega Exported = false
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class MyFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        AndroidNotificationManager androidNotification = new AndroidNotificationManager();
+
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+
+            androidNotification.CrearNotificacionLocal(message.GetNotification().Title, message.GetNotification().Body);
+        }
+
+        public override void OnNewToken(string token)
+        {
+            base.OnNewToken(token);
+
+            Preferences.Set("TokenFirebase", token);
+            sedRegisterToken(token);
+        }
+
+        public void sedRegisterToken(string token)
+        {
+            //Tu código para registrar el token a tu servidor y base de datos
+        }
+    }
+}
